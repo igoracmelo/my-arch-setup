6bc379681d0961828cae03c372bfbddbb80abe07
Igor Melo <igoracmelo@protonmail.com>
2023-02-09 17:19:18 +0000

    wip: add comments and make executable

--- setup.sh
+++ setup.sh
@@ -1,3 +1,6 @@
+#!/bin/bash
+
+# yay
 if ! [ -x "$(command -v yay)" ]; then
 	sudo pacman -S git base-devel
 	git clone https://aur.archlinux.org/yay-bin
@@ -35,19 +38,23 @@ pkgs=(
 
 yay -S --needed ${pkgs[@]}
 
+# link vim -> nvim
 if ! [ -x "$(command -v vim)" ]; then
 	sudo ln -s /usr/bin/nvim /usr/bin/vim 
 fi
 
+# lunarvim
 if ! [ -x "$(command -v lvim)" ]; then
 	LV_BRANCH='release-1.2/neovim-0.8' bash <(curl -s https://raw.githubusercontent.com/lunarvim/lunarvim/fc6873809934917b470bff1b072171879899a36b/utils/installer/install.sh)
 fi
 
+# yarn
 if ! [ -x "$(command -v yarn)" ]; then
 	# i hate to do sudo npm but the solution is not very practical
 	sudo npm i -g yarn 
 fi
 
+# oh-my-zsh
 if ! [ -d ~/.oh-my-zsh ]; then
 	curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh
 fi
